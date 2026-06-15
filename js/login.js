9990d521ba75fa836dcff28ce92df2ab8fe116a1
MATHEUS HENRY DOS SANTOS <25204051@CFP127ALUNO.LOCAL>
2026-06-15 16:56:20 -0300

    feat: Começo da criação da página administrativa após o login

---
+++ js/login.js
@@ -0,0 +1,33 @@
+'use strict'
+
+//Todos os imports
+
+
+
+//Tela de Login
+
+function validarEmailESenha(email, senha){
+
+}
+
+function validarLogin(){
+    const email = document.getElementById("email").value
+    const senha = document.getElementById("senha").value
+
+    validarEmailESenha(email,senha)
+} 
+
+//Tela de Adicionar
+
+
+
+//Tela de Exibir
+
+
+
+//Tela de Atualizar
+
+
+
+//Tela de Alterar Cadastro
+
